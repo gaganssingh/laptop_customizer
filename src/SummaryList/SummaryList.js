17874e3284e97144d5f378bee02e283f4b7aa95f
Gagan Singh <gagansingh.dev@outlook.com>
2019-10-17 19:54:04 -0400

    Created a child component SummaryOption.js for SummaryList.js

--- src/SummaryList/SummaryList.js
+++ src/SummaryList/SummaryList.js
@@ -1,9 +1,5 @@
 import React from "react";
-
-const USCurrencyFormat = new Intl.NumberFormat("en-US", {
-	style    : "currency",
-	currency : "USD"
-});
+import SummaryOption from "../SummaryOption/SummaryOption";
 
 export default function SummaryList(props) {
 	return Object.keys(props.selected).map((feature, idx) => {
@@ -11,10 +7,8 @@ export default function SummaryList(props) {
 		const selectedOption = props.selected[feature];
 
 		return (
-			<div className="summary__option" key={featureHash}>
-				<div className="summary__option__label">{feature} </div>
-				<div className="summary__option__value">{selectedOption.name}</div>
-				<div className="summary__option__cost">{USCurrencyFormat.format(selectedOption.cost)}</div>
+			<div>
+				<SummaryOption feature={feature} featureHash={featureHash} selectedOption={selectedOption} />
 			</div>
 		);
 	});
